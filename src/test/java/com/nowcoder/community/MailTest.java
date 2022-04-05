c164078b96ac72499ad3a4dbbce32755650f4380
王志成-HITSZ <1244533796@qq.com>
2022-04-05 12:58:29 +0800

    注册功能

---
+++ src/test/java/com/nowcoder/community/MailTest.java
@@ -0,0 +1,31 @@
+package com.nowcoder.community;
+
+import com.nowcoder.community.util.MailClient;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class MailTest {
+    @Autowired
+    private MailClient mailClient;
+
+    @Autowired
+    private TemplateEngine templateEngine;
+    @Test
+    public void testMail() {
+        mailClient.sendMail("as911520@icloud.com", "test", "hello");
+
+    }
+    @Test
+    public void testHtmlMail() {
+        Context context = new Context();
+        context.setVariable("username", "sunday");
+        String content = templateEngine.process("/mail/demo", context);
+        mailClient.sendMail("as911520@icloud.com", "html", content);
+    }
+}
